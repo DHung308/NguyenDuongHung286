d8b23c72fa6e72ac3485baa911917db87b9f91cd
DHung308 <nguyenduonghung30082001@gmail.com>
2023-01-04 08:11:51 +0700

    First_commit_NDH286

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenDuongHung286.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
